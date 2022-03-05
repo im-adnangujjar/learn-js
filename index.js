3d7c69029cd4a989111d87f4b3f966b21cf9e635
im-adnangujjar <adnan.yaqoob.gujar@gmail.com>
2022-03-05 11:49:30 +0400

    learnjs

---
+++ index.js
@@ -0,0 +1,339 @@
+// var name = "Adnan"
+// var age = 23
+// var roll = 456789
+// console.log(age ,roll);
+// console.log(name);
+//  var num1 = 15
+//  var num2 = 85
+//  var result = num1+num2
+//  console.log("result",result);
+
+// const { get } = require("jquery");
+
+// const { number } = require("prop-types");
+
+
+//  var name = "Adnan"
+//  var Name = "Yaqoob"
+// //  var fullname = name +" " + Name
+//  console.log("name", name , Name);
+//  var firstName = 1234.43
+//   var secound ="false"
+//  console.log( "secound", typeof (secound) );
+//  var num1 = "7.23"
+//  var num2 = 85
+//  var num3 = 45
+//  var num4 = 80
+//  num1 = num1.toString()
+
+
+//  console.log("num1",typeof(num1));
+
+//  console.log(num1+num2+num3+num4);
+
+//  var num1 = 23
+//  var num2 = 25
+//  var num3 = 60
+//  var num4 = 20
+// //  statmenat
+// var finalstatment = num1 >= num2 && num3 <= num4 || num1 < num2 && num3 != num4 && 2 < 5
+// console.log("finalstatment" , finalstatment);
+//  num1 >= num2 && num3 <= num4
+//  console.log( num1 >= num2 && num3 <= num4);
+//  console.log(num1 < num2 && num3 != num4 && 2 < 5);
+
+
+
+
+
+
+// console.log("line39");
+// console.log("line46");
+// console.log("line47");
+// console.log("line48");
+// console.log("line49");
+// console.log("line50");
+// if (10 !=10 || 40 < 30) {
+//     console.log("line53");    
+//     console.log("line54");    
+
+// }
+// else{
+// console.log("line55");
+// console.log("line56"); 
+// console.log("line57");
+// }
+// // write a js program to compare two numbers weather they are equal or not
+// var num1 = 10
+// var num2 = 20 
+// if (num1 != num2) {console.log
+//     ("both num1 and num2 are equal numbers");  
+// }
+// else{
+//     console.log("both num1 and num2 are not equal numbers");
+// }
+// var num1 = 8
+// var num2 = 5
+// if ( 4 % 3==0) {
+//     console.log("number is even");
+// }
+// else{
+//     console.log("number is odd");
+// }
+// score < 50 ,fail
+// score > 50 , < 60 ,c 
+// score > 60 , < 70 ,B
+// score > 70 , < 80 ,A
+// score > 80 , < 90 ,A+
+// var score = 101;
+// if (score < 50) {
+//     console.log("Fail");
+// }
+//  else if (score >= 50 && score < 60) {
+//     console.log("student grade c");
+// }
+//  else if (score >= 60 && score < 70) {
+//     console.log("student grade B");
+// }
+// else if (score >= 70 && score < 80) {
+//     console.log("student grade A");
+// }
+// else if(score >= 80 && score < 100) {
+//     console.log("student grade A+");
+// }
+// else{
+//     console.log(" not found");
+// }
+// loop
+
+
+// for (let index = 4; index <= 10; index++){
+//     console.log( "index",index);
+// }
+// var num1 =0
+// while (num1 < 50) {
+//     console.log("while loop" );
+//      num1++
+// }
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+// var numbers = [1,2,3,4,5,6,0.4,8,9,10,11]
+// console.log(numbers.length);
+// console.log("numbers" , numbers[11])
+// // var names   = ["Adnan","Gujjar","Ahmad","Ali","Hassan"]
+// // for (let index = 0; index < names.length; index++) {
+// //     console.log("index",names[index]);
+
+// // }
+// var names   = ["Adnan","Gujjar","Ahmad","Ali","Hassan"  ]
+// console.log("neams.length",names );
+
+// names.push("buda gujjar")
+// console.log(names);
+// names.pop()
+// console.log(names);
+// new Datevar date = 
+// console.log(date);[5:04 PM, 1/5/2022] Hasnan Bahi: Assignment#07
+// Dear Students / Brothers.
+// Web and Mobile Application Development, Batch-04 Section-C
+// Requirements:
+// 1- Please deploy into surge, and create online link
+// 2- Take input through DOM form
+// 3- Show output of all programs in HTML Tags eg, h1,h2,p
+// 4- Submit the assignment at link below
+// Programs:
+// If-else exercise
+
+
+
+
+// 
+
+// 
+
+// 9. Write a js program to input any character and check whether it is alphabet, digit or special character.
+// 11. 
+// 12. Write a js program to input month number and print number of days in that month.
+// 13. Write a js program to count total number of notes in given amount.
+// 
+// 15. Write a js program to input basic salary of an employee and calculate its Gross salary according to following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+// 16. Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For unit above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill
+// Deadline: 08/12/2021!
+// Submit: https://forms.gle/vnVFg4dRWFADLJK2A
+// Regards
+// Naveed Sarwar
+// 1. Write a js program to find maximum between two numbers.
+// var num1 = parseInt(prompt("enter first number"));
+// var num2 = parseInt(prompt("enter second number"))
+// if (num1 > num2) {
+//     console.log('num1 is greater than num2' );
+// } else {
+//     console.log('num2 is greater than num1');
+// }
+// 2. Write a js program to find maximum between three numbers.
+// var num3 = parseInt(prompt("enter first number"));
+// var num4 = parseInt(prompt("enter second number"));
+// var num5 = parseInt(prompt("enter thired number"));
+// if (num3 > num4 = num3 & num4 > num5){
+//     console.log("num3 is greater then num4 and num5");
+// }  else  {
+//     console.log("num4 and num5 is greater then num3");
+// }
+// 3. Write a js program to check whether a number is negative, positive or zero.
+// var num = parseInt(prompt('enter a number'));
+// if (num < 0) {
+//     console.log('number is negetive');
+// }
+// if (num > 0) {
+//     console.log('number is positive');
+// }
+// if (num === 0) {
+//     console.log('number is zero'); 
+// }
+// 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+// var num = parseInt(prompt("enter the number"))
+// if (num % 5 == 0) {
+//     console.log("divisibl number");
+// } else if(num % 11 == 0){
+//     console.log(" divisible number");
+// }else{
+//     console.log("not divisible number");
+// }
+// 5. Write a js program to check whether a number is even or odd.
+// var num7 = 10;
+
+// if (num7 / 2 === 1) {
+//     console.log("even number",num7);
+// } else  {
+//     console.log("odd number" , num7);
+// }
+// 6. Write a js program to check whether a year is leap year or not.
+// var year = parseInt(prompt("enter input1"))
+// var febDays = new Date(year, 2, 0).getDate();
+// if (febDays === 29) {
+//     console.log('Year is leap yaer');
+// } else {
+//     console.log('Year is not a leap yaer');
+
+// }
+// // 7. Write a js program to check whether a character is alphabet or not.
+// var input = parseInt(prompt("enter input"));
+// var num = Number.isInteger(input);
+// console.log(num);
+// if (num) {
+//     console.log("not alphabet");
+// } else {
+//     console.log("alphabet");
+// }
+// 8. Write a js program to input any alphabet and check whether it is vowel or consonant.
+// var num =prompt("enter Input")
+
+// if (num==="a"||num==="e"||num==="i"||num==="o"||num==="u") {
+//     console.log('alphabet is vowel');
+// } 
+// else {
+//     console.log('alphabet is  consonant');
+// }
+
+// 10. Write a js program to check whether a character is uppercase or lowercase alphabet.
+// var alphabet=prompt("enter input")
+// var upperCase=alphabet.toLocaleUpperCase()
+// if (upperCase == alphabet) {
+//     console.log("uppercase");
+// } else {
+//     console.log("lowercase");
+// }
+// 11. Write a js program to input week number and print week day.
+
+
+// var input=prompt("Enter Input")
+// var date= new Date(input);
+// console.log(date);
+// var getDay=date.getDay()
+// console.log(getDay);
+// let daysName = ["Sun","Mon","Tues", "Wed","Thus","Fri","Sat"];
+
+// if (getDay === 0) {
+//     console.log("sunday");
+// }
+// else if (getDay === 1) {
+//     console.log("Monday");
+// }
+// else if (getDay === 2)
+//     console.log("tuseday");
+// else  if (getDay === 3) {
+//     console.log("wednesday");
+// }
+// else if (getDay === 4) {
+//     console.log("thursday");
+// }
+// else if (getDay === 5) {
+//     console.log("Friday");
+// }
+// else if (getDay === 6) {
+//     console.log("satuarday");
+// } 
+// else{
+//     console.log("not a day number");
+// }
+
+
+//  var string ="i am adnan "
+
+//  var replacedstring =string.toLowerCase()
+//  console.log(replacedstring);
+//  var day = "monday"
+//  console.log(day);
+//  if( day.toLocaleUpperCase() === "Monday") {
+//      console.log( "today is Monday");
+//  } else {
+//      console.log("today is not Monday");
+//  }
+// 14. Write a js program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+// var phy=parseInt(prompt("enter your physics marks"))
+// var che=parseInt(prompt("enter your chemistry marks"))
+// var bio=parseInt(prompt("enter your biology marks"))
+// var mat=parseInt(prompt("eter your mathematics marks"))
+// var com=parseInt(prompt("enter your computer marks "))
+// var sum= phy+che+bio+mat+com
+// console.log(sum);
+// var per = sum/500*100
+// console.log("per",per);
+// if ( per >= 90) {
+//     console.log("Grade A");
+// } 
+// else if ( per >= 80 && per < 90 ) {
+//     console.log("Grade B");
+// }
+// else if ( per >= 70 && per < 80){
+//     console.log("Grade C");
+// }
+// else if ( per >= 60 && per < 70) {
+//     console.log("Grade D");
+// }
+// else if ( per >= 50 && per < 60 ) {
+//     console.log("Grade E");
+// }
+// else {
+//     console.log("Grade F");
+// }
